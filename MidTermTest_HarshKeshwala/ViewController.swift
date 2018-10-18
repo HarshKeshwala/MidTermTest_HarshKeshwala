6f266d42207e8fea763af6cd60d6e31610091177
Harsh Keshwala <harshkeshwala@harshs-macbook-pro.local>
2018-10-18 19:01:56 -0400

    Initial Commit

---
+++ MidTermTest_HarshKeshwala/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MidTermTest_HarshKeshwala
+//
+//  Created by Harsh Keshwala on 2018-10-18.
+//  Copyright © 2018 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
